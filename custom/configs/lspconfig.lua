c2fa242f310978a37865855d4cab381ae2a45518
Henk Hornschuh <h.hornschuh@basecom.de>
2024-02-22 16:01:51 +0100

    twig

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "clangd", "volar", "tailwindcss" }
+local servers = { "html", "cssls", "clangd", "volar", "tailwindcss", "phpactor", "twiggy_language_server" }
 
 lspconfig.volar.setup{
   filetypes = {"typescript", "javascript, javascriptreact", "typescriptreact", "vue", "json"}
